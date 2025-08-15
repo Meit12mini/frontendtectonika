94c6f288cac2da5e9237be0cb8ee04acfb38582f
meit <zhora.makarov.2005@yandex.ru>
2025-08-15 15:31:57 +0900

    glen23

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -1,3 +1,34 @@
+// src/services/geminiService.ts
+// import type { Answers, GeminiResponse } from '../types';
+
+// export const processLead = async (answers: Answers, phone: string): Promise<GeminiResponse> => {
+//   try {
+//     const response = await fetch('https://backendtectonika.onrender.com/api/lead', {
+//       method: 'POST',
+//       headers: { 'Content-Type': 'application/json' },
+//       body: JSON.stringify({ answers, phone })
+//     });
+
+//     if (!response.ok) {
+//       const errorData = await response.json();
+//       throw new Error(errorData.error || 'Ошибка сервера');
+//     }
+
+//     const data = await response.json();
+//     return data as GeminiResponse;
+
+//   } catch (error: any) {
+//     console.error('Ошибка при отправке заявки на бэк:', error);
+//     throw new Error(error.message || 'Не удалось обработать заявку');
+//   }
+// };
+
+
+
+
+
+
+
 // import { GoogleGenAI, Type } from "@google/genai";
 // import type { Answers, GeminiResponse } from '../types';
 
